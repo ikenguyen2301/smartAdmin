d66efc43a15d900154bcbc9bd67b85455a6be562
ike.nguyen <ike.nguyen@bstarsolution.com>
2017-01-03 15:38:14 +0700

    Update notification

--- app/beacon/BeaconDetailCtrl.js
+++ app/beacon/BeaconDetailCtrl.js
@@ -21,13 +21,25 @@ angular.module('app.beacon').controller('BeaconDetailCtrl', function ($scope, $i
         if(!data.objectId) {
             beaconService.post(data, function (data) {
                 // Implement toaster here when create successful
-                console.log("success")
+                 $.smallBox({
+                            title: "Create Notification ",
+                            content: "Created successful!",
+                            color: "#659265",
+                            iconSmall: "fa fa-check fa-2x fadeInRight animated",
+                            timeout: 4000
+                        });
                 $state.go("app.beacon");
             });
         } else {
             beaconService.put(data, function (data) {
                 // Implement toaster here when create successful
-                console.log("success")
+                $.smallBox({
+                            title: "Update Notification",
+                            content: "Updated successful!",
+                            color: "#659265",
+                            iconSmall: "fa fa-check fa-2x fadeInRight animated",
+                            timeout: 4000
+                        });
                 $state.go("app.beacon");
             });
         }
