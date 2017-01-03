ca6c9f7e6114f16bfefe27574d3d3378da5ef942
ike.nguyen <ike.nguyen@bstarsolution.com>
2017-01-03 19:39:24 +0700

    update required fields

--- app/beacon/BeaconDetailCtrl.js
+++ app/beacon/BeaconDetailCtrl.js
@@ -12,36 +12,47 @@ angular.module('app.beacon').controller('BeaconDetailCtrl', function ($scope, $i
     };
 
     if(!$stateParams.id){
-        $scope.itemDetail ={};
+        $scope.itemDetail ={"url":"http://transx.com/eddystonet","firmware":"4.1","profile" : 'iBeacon',"alias": 'TransX'};
     }else{
         $scope.getDetail();
     }
 
-    $scope.save = function(data){
-        if(!data.objectId) {
-            beaconService.post(data, function (data) {
-                // Implement toaster here when create successful
-                 $.smallBox({
-                            title: "Create Notification ",
-                            content: "Created successful!",
-                            color: "#659265",
-                            iconSmall: "fa fa-check fa-2x fadeInRight animated",
-                            timeout: 4000
-                        });
-                $state.go("app.beacon");
-            });
-        } else {
-            beaconService.put(data, function (data) {
-                // Implement toaster here when create successful
-                $.smallBox({
-                            title: "Update Notification",
-                            content: "Updated successful!",
-                            color: "#659265",
-                            iconSmall: "fa fa-check fa-2x fadeInRight animated",
-                            timeout: 4000
-                        });
-                $state.go("app.beacon");
-            });
+    $scope.save = function(data, isValid){
+        if(isValid){
+            if(!data.objectId) {
+                beaconService.post(data, function (data) {
+                    // Implement toaster here when create successful
+                     $.smallBox({
+                                title: "Create Notification ",
+                                content: "Created successful!",
+                                color: "#659265",
+                                iconSmall: "fa fa-check fa-2x fadeInRight animated",
+                                timeout: 4000
+                            });
+                    $state.go("app.beacon");
+                });
+                } else {
+                beaconService.put(data, function (data) {
+                    // Implement toaster here when create successful
+                    $.smallBox({
+                                title: "Update Notification",
+                                content: "Updated successful!",
+                                color: "#659265",
+                                iconSmall: "fa fa-check fa-2x fadeInRight animated",
+                                timeout: 4000
+                            });
+                    $state.go("app.beacon");
+                });
+            }
+        }else{
+            //  $.smallBox({
+            //     title: "Error ",
+            //     content: "Form have error !",
+            //     color: "#659265",
+            //     iconSmall: "fa fa-check fa-2x fadeInRight animated",
+            //     timeout: 4000
+            // });
         }
+        
     }
 });
